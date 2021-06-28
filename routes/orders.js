c8a3cefe63cba99ffc9b5cdcff678c66d7244db1
Atizaz Ahsan <atizazahsan05@gmail.com>
2021-06-28 15:29:09 +0500

    Order Routes
    order routes created

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,48 @@
 const express = require('express');
+const mongoose  = require('mongoose');
 const route = express.Router();
 
+const Order = require('../model/order');
+
+route.get('/' , (req, res, next) => {
+    Order.find()
+    .exec()
+    .then(docs => {
+        res.status(200).json({
+            message : "All orders has been fetched" ,
+            orders : docs
+        })
+    })
+    .catch( err => {
+        res.status(500).json({
+            error : err
+        })
+    })
+})
 // Show all Orders
 
-route.get('/' , (req , res , next) => {
-    res.send("All Orders Here")
+route.post('/' , (req , res , next) => {
+    const order = new Order({
+       _id : mongoose.Types.ObjectId(),
+       quantity : req.body.quantity ,
+       product : req.body.productId
+   })
+
+   order.save()
+   .then(result =>{
+       console.log(result);
+       res.status(200).json({
+           message : "Order was posted",
+           submitedOrder : order
+       })
+   })
+   .catch(err =>{
+       console.log(err);
+       res.status(200).json({
+           error : err
+       })
+   })
+   
 })
 
 module.exports = route ;
--- routes/products.js
+++ routes/products.js
@@ -92,4 +92,19 @@ route.get('/:productId' , (req , res , next) => {
     })
 })
 
+// delete
+
+route.delete('/:productId' , (req , res , next) => {
+    const id = req.params.productId
+    Product.remove({_id : id})
+    .exec()
+    .then(result => { 
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json(
+            {error : err}
+            )
+    })
+})
 module.exports = route ;
